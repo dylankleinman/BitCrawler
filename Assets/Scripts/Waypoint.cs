72c86fc87cea34098fe229cbf11b55d4716b56e4
Dylan Kleinman <dylankkleinman@gmail.com>
2019-09-26 19:48:58 -0700

    Added cube assets, enemy, and tower with follow function

---
+++ Assets/Scripts/Waypoint.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour
+{
+    [SerializeField] Color exploredColor;
+
+    Vector2Int gridPos;
+    const int gridSize = 10;
+    public bool isExplored = false;
+    public Waypoint exploredFrom;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public Vector2Int GetGridPos()
+    {
+        return new Vector2Int(
+            Mathf.RoundToInt(transform.position.x / 10f),
+            Mathf.RoundToInt(transform.position.z / 10f)
+        );
+    }
+
+    public void SetTopColor(Color color)
+    {
+        MeshRenderer topMeshRenderer =  transform.Find("Top").GetComponent<MeshRenderer>();  //looks at children
+        topMeshRenderer.material.color = color;
+    }
+
+    public int GetGridSize()
+    {
+        return gridSize;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isExplored)
+        {
+            //neighbour.SetTopColor(Color.blue);  // todo set top color
+        }
+    }
+}
